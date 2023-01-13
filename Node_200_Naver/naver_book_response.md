96166aa0d8919deec0b75878e5072cd6f3aaa0ac
callor <callor@callor.com>
2023-01-13 14:56:47 +0900

    naver_db

---
+++ Node_200_Naver/naver_book_response.md
@@ -0,0 +1,9 @@
+title String 책 제목
+link String 네이버 도서 정보 URL
+image String 섬네일 이미지의 URL
+author String 저자 이름
+discount Integer 판매 가격. 절판 등의 이유로 가격이 없으면 값을 반환하지 않습니다.
+publisher String 출판사
+isbn Integer ISBN
+description String 네이버 도서의 책 소개
+pubdate dateTime 출간일
--- Node_200_Naver/routes/book.js
+++ Node_200_Naver/routes/book.js
@@ -1,8 +1,42 @@
 import express from "express";
+import NAVER from "../config/naver_config.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  res.render("book/list");
+  const search = req.query.search;
+  if (!search) {
+    return res.send("검색어를 입력해야 합니다");
+  }
+
+  const naverFetchOption = {
+    method: "GET",
+    headers: {
+      [NAVER.CLIENT_ID.KEY]: NAVER.CLIENT_ID.VALUE,
+      [NAVER.CLIENT_SECRET.KEY]: NAVER.CLIENT_SECRET.VALUE,
+    },
+  };
+  const queryString = `${NAVER.BOOK_URL_JSON}?query=${search}`;
+  let response = null;
+  try {
+    response = await fetch(queryString, naverFetchOption);
+  } catch (err) {
+    console.log("fetch error", err);
+    return res.json(response.json());
+  }
+
+  let result = null;
+  try {
+    result = await response.json();
+  } catch (err) {
+    console.log("JSON 변환 오류", err);
+    return res.send("JSON 변환 과정에서 문제 발생");
+  }
+  // return res.json(result);
+
+  const book_list = result.items.map((item) => {
+    return { ...item, author: item.author.replaceAll("^", " ") };
+  });
+  return res.render("book/list", { BOOKS: book_list });
 });
 
 export default router;
